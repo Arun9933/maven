69d1b9286fa01bcd54762b189c33a0224b334240
arunmujalge2910@gmail.com <asmujalge333@gmail.com>
2023-09-12 16:16:25 +0530

    add

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,4 +16,12 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+  
+  public String greet2(String someone) {
+	    return String.format("Hello, %s!", someone);
+	  }
+  
+  public String greet3(String someone) {
+	    return String.format("Hello, %s!", someone);
+	  }
 }
